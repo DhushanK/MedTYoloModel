4d66b8d6a758c1d5558c06bbe46995a9b4abed6b
Dhushan Kiritharan <161161235+DhushanK@users.noreply.github.com>
2025-04-02 12:36:36 -0400

    Update yolo_main.py

--- yolo_main.py
+++ yolo_main.py
@@ -2,10 +2,14 @@ from ultralytics import YOLO
 import cv2
 from Vehicle_Class import Vehicle
 
+import time
+
 # model there is also yolo11n yolo11s yolo11m yolo11l yolo11x, dunno which is best
 model = YOLO("yolo11n.pt")
 names = model.names
 # print(names)
+ESP32_URL = "http://192.168.1.100/video"  # change as needed
+# cap = cv2.VideoCapture(ESP32_URL)
 cap = cv2.VideoCapture(0)  # Change to 0 for camera
 width = 1280
 height = 720
@@ -33,14 +37,29 @@ def get_vehicle(box):
             # right side of the screen, use middle left pixel for position
             position = (x1, centre[1])
 
-        vehicle = Vehicle(v_id, position, area)
+        vehicle = Vehicle(v_id, position, area, centre)
 
         return vehicle
     return None
 
 
+def safe_warning():
+    pass
+    # do warning stuff
+    # lat, lon = 0, 0
+    # if get_distance(lat, lon) > 40:
+    #     pass
+
+
 def main():
+    # -------------------
+    # lat, lon = 0, 0  # change accordingly
+    # while get_distance(lat, lon) > 40:
+    #     pass
+    # -------------------
+
     while cap.isOpened():
+        safe_time = time.time()
         success, frame = cap.read()
         if success:
             results = model.track(
@@ -52,8 +71,15 @@ def main():
             for box in boxes:
                 vehicle = get_vehicle(box)
                 if vehicle:
-                    warning = vehicle.get_warning()
-                    print(warning, end="")
+                    safe_check, warning_message = vehicle.get_warning()
+
+                    if not safe_check:
+                        safe_time = time.time()
+                        print(warning_message)
+
+            if time.time() - safe_time >= 5:
+                safe_warning()
+                safe_time = time.time()
 
             cv2.imshow("YOLO11 Tracking", annotated_frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -61,7 +87,7 @@ def main():
         else:
             # Break the loop if the end of the video is reached
             break
-
+    cap.release()
 
 if __name__ == "__main__":
     main()
